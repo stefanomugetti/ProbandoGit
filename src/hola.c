1063fb35c49702f0e69088d2a7f0e9417b98c18a
Stefano Mugetti <stefanomugetti7@gmail.com>
2021-03-18 21:42:20 -0300

    mi primer commit

---
+++ src/hola.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : hola.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+
+
+	return 0;
+}
